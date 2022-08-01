fb8cfb8f2484617d9a5660499ca6c37073d065a1
Toshimichi0915 <main@toshimichi.net>
2022-08-02 05:09:21 +0900

    Rewrite code / Omit duplicated stacktrace

--- src/main/java/net/toshimichi/nakuru2/DiscordHandler.java
+++ src/main/java/net/toshimichi/nakuru2/DiscordHandler.java
@@ -1,8 +1,12 @@
-package jp.mini_game.nakuru2;
+package net.toshimichi.nakuru2;
 
-import net.dv8tion.jda.core.entities.TextChannel;
+import net.dv8tion.jda.api.entities.TextChannel;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.logging.Formatter;
@@ -12,25 +16,52 @@ import java.util.logging.LogRecord;
 
 public class DiscordHandler extends Handler {
 
+    private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
     private static final int LENGTH_LIMIT = 1000;
 
     private final Set<Level> levels;
     private final TextChannel output;
     private final Formatter formatter;
+    private final HashSet<String> messages;
 
     public DiscordHandler(Set<Level> levels, TextChannel output, Formatter formatter) {
         this.levels = levels;
         this.output = output;
         this.formatter = formatter;
+        this.messages = new HashSet<>();
+    }
+
+    public static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for (int j = 0; j < bytes.length; j++) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = HEX_ARRAY[v >>> 4];
+            hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
+        }
+        return new String(hexChars);
     }
 
     @Override
     public void publish(LogRecord logRecord) {
         // レベルを満たさなかったらreturn
-        if(!levels.contains(logRecord.getLevel())) return;
+        if (!levels.contains(logRecord.getLevel())) return;
+
+        MessageDigest digest;
+
+        try {
+            digest = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        String md5 = bytesToHex(digest.digest(logRecord.getMessage().getBytes(StandardCharsets.UTF_8)));
+        if (messages.contains(md5)) return;
+        messages.add(md5);
+
         // キュー内に入れて、flushで送信する
         output.sendMessage("**" + logRecord.getLevel().getName() + "**").queue();
-        separate(formatter.format(logRecord)).forEach(m->output.sendMessage(m).queue());
+        separate(formatter.format(logRecord)).forEach(m -> output.sendMessage(m).queue());
     }
 
     @Override
@@ -43,6 +74,7 @@ public class DiscordHandler extends Handler {
 
     /**
      * メッセージをDiscordで送信可能なように文字列を分割します
+     *
      * @param message 分割したいメッセージ
      * @return 分割されたメッセージ
      */
@@ -53,7 +85,7 @@ public class DiscordHandler extends Handler {
         StringBuilder builder = new StringBuilder("```");
 
         // 行ごとの処理
-        for(int lineNumber = 0; lineNumber < lines.length; lineNumber++) {
+        for (int lineNumber = 0; lineNumber < lines.length; lineNumber++) {
             String line = lines[lineNumber];
 
             // 現在の長さを調べる
@@ -61,7 +93,7 @@ public class DiscordHandler extends Handler {
                     + line.codePointCount(0, line.length());
 
             // 長さの限界を超えていなくて、最後の行でなければ
-            if(count < LENGTH_LIMIT&& lineNumber + 1 != lines.length) {
+            if (count < LENGTH_LIMIT && lineNumber + 1 != lines.length) {
                 builder.append(line);
                 builder.append('\n');
             } else {
--- src/main/java/net/toshimichi/nakuru2/Nakuru2Plugin.java
+++ src/main/java/net/toshimichi/nakuru2/Nakuru2Plugin.java
@@ -1,10 +1,10 @@
-package jp.mini_game.nakuru2;
+package net.toshimichi.nakuru2;
 
-import net.dv8tion.jda.core.AccountType;
-import net.dv8tion.jda.core.JDA;
-import net.dv8tion.jda.core.JDABuilder;
-import net.dv8tion.jda.core.OnlineStatus;
-import net.dv8tion.jda.core.entities.TextChannel;
+import net.dv8tion.jda.api.AccountType;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.TextChannel;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -25,14 +25,12 @@ public class Nakuru2Plugin extends JavaPlugin {
             saveDefaultConfig();
 
             //JDAの生成
-            jda = new JDABuilder(AccountType.BOT)
-                    .setToken(getConfig().getString("token"))
-                    .setStatus(OnlineStatus.ONLINE)
-                    .buildBlocking();
+            jda = JDABuilder.createDefault(getConfig().getString("token")).build();
+            jda.awaitReady();
 
             //DiscordHandlerの生成
             Set<Level> levels = new HashSet<>();
-            getConfig().getStringList("levels").forEach(l->levels.add(Level.parse(l)));
+            getConfig().getStringList("levels").forEach(l -> levels.add(Level.parse(l)));
             TextChannel channel = jda.getTextChannelById(getConfig().getString("channel"));
             handler = new DiscordHandler(levels, channel, new SimpleFormatter());
             Bukkit.getLogger().addHandler(handler);
@@ -46,7 +44,8 @@ public class Nakuru2Plugin extends JavaPlugin {
     @Override
     public void onDisable() {
         Bukkit.getLogger().removeHandler(handler);
-        if(jda != null)
+        if (jda != null) {
             jda.shutdown();
+        }
     }
 }
